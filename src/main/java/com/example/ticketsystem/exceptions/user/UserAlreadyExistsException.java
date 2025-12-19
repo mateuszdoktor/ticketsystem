8f8295754667f17c6c27c1978d9ff9c1e381e406
mateuszdoktor <matdoktor3@gmail.com>
2025-12-19 13:22:37 +0100

    add UserService, exceptions, and SecurityConfig with BCrypt

---
+++ src/main/java/com/example/ticketsystem/exceptions/user/UserAlreadyExistsException.java
@@ -0,0 +1,7 @@
+package com.example.ticketsystem.exceptions.user;
+
+public class UserAlreadyExistsException extends RuntimeException {
+    public UserAlreadyExistsException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
---
+++ src/main/java/com/example/ticketsystem/service/UserService.java
@@ -0,0 +1,54 @@
+package com.example.ticketsystem.service;
+
+import com.example.ticketsystem.dto.user.UserCreateDto;
+import com.example.ticketsystem.entity.User;
+import com.example.ticketsystem.exceptions.user.UserAlreadyExistsException;
+import com.example.ticketsystem.exceptions.user.UserNotFoundException;
+import com.example.ticketsystem.repository.UserRepository;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder) {
+        this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
+    }
+
+    @Transactional
+    public User createUser(UserCreateDto userCreateDto) {
+        User user = new User();
+        user.setUsername(userCreateDto.getUsername());
+        user.setUserRole(userCreateDto.getRole());
+        user.setPassword(bCryptPasswordEncoder.encode(userCreateDto.getPassword()));
+
+        try {
+            return userRepository.save(user);
+        } catch (DataIntegrityViolationException exception) {
+            throw new UserAlreadyExistsException("User with username: " + userCreateDto.getUsername() + ", already exists.", exception);
+        }
+    }
+
+    @Transactional(readOnly = true)
+    public User findById(Long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException("User with id: " + id + ", not found."));
+    }
+
+    @Transactional(readOnly = true)
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElseThrow(() -> new UserNotFoundException("User with username: " + username + ", not found."));
+    }
+
+    @Transactional(readOnly = true)
+    public Page<User> findAll(Pageable pageable) {
+        return userRepository.findAll(pageable);
+    }
+}
